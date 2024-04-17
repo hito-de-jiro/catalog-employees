9b67fcba00819e7eb52a30d555d3c3edad788cbe
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-04-17 17:13:40 +0300

    feat: added catalog/seed.py

---
+++ app/catalog/seed.py
@@ -0,0 +1,14 @@
+from catalog.models import Employee
+from django_seed import Seed
+
+seeder = Seed.seeder()
+
+seeder.add_entity(Employee, 100, {
+    'full_name': lambda x: seeder.faker.name(),
+    'position': lambda x: seeder.faker.job(),
+    'hire_date': lambda x: seeder.faker.date_this_century(),
+    'salary': lambda x: seeder.faker.random_number(digits=5),
+    'manager': lambda x: Employee.objects.order_by('?').first()
+})
+
+seeder.execute()
