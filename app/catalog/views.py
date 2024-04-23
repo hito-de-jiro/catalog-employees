5474746c2b2a86313970c817874a5c167394a00d
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-04-23 10:24:50 +0300

    feat: added view for employee_list

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -6,3 +6,10 @@ from .models import Employee
 def employee_hierarchy(request):
     employees = Employee.objects.all()
     return render(request, 'catalog/employees.html', {'employees': employees})
+
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    sort_by = request.GET.get('sort_by', 'full_name')
+    employees = employees.order_by(sort_by)
+    return render(request, 'employee_list.html', {'employees': employees})
