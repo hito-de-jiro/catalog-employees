31d233ac50e5fd8a12a65c71d5aec025a97ba2db
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-04-23 12:26:46 +0300

    feat: added js lib DataTable

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import employee_hierarchy, employee_list
+from .views import employee_hierarchy, employee_list, employee_search, employee_data
 
 urlpatterns = [
     path('employee_hierarchy/', employee_hierarchy, name='employee_hierarchy'),  # http://127.0.0.1:8000/catalog/employee_hierarchy/
     path('employees/', employee_list, name='employee_list'),  # http://127.0.0.1:8000/catalog/employees/
+    path('employees/', employee_search, name='employee_search'),  # http://127.0.0.1:8000/catalog/employees/
+    path('employees-data/', employee_data, name='employee_data'),  # http://127.0.0.1:8000/catalog/employees-data/
 ]
--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from .models import Employee
@@ -13,3 +14,21 @@ def employee_list(request):
     sort_by = request.GET.get('sort_by', 'full_name')
     employees = employees.order_by(sort_by)
     return render(request, 'catalog/employee_list.html', {'employees': employees})
+
+
+def employee_search(request):
+    employees = Employee.objects.all()
+    search_query = request.GET.get('search_query')
+    if search_query:
+        employees = employees.filter(
+            Q(full_name__icontains=search_query) |
+            Q(hire_date__icontains=search_query) |
+            Q(salary__icontains=search_query)
+        )
+    return render(request, 'catalog/employee_list.html', {'employees': employees})
+
+
+def employee_data(request):
+    employees = Employee.objects.all()
+
+    return render(request, 'catalog/employee-data.html', {'employees': employees})
