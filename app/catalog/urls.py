d4e2cf7146c444c09e2299657face3faf2c0e2e0
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-04-26 12:21:04 +0300

    refactor(project): refactor views.py, redundant part removed

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import employee_hierarchy, employee_list, employee_search, employee_data
+from .views import (
+    employee_list,
+)
 
 urlpatterns = [
-    path('employee_hierarchy/', employee_hierarchy, name='employee_hierarchy'),  # http://127.0.0.1:8000/catalog/employee_hierarchy/
-    path('employees/', employee_list, name='employee_list'),  # http://127.0.0.1:8000/catalog/employees/
-    path('employees/', employee_search, name='employee_search'),  # http://127.0.0.1:8000/catalog/employees/
-    path('employees-data/', employee_data, name='employee_data'),  # http://127.0.0.1:8000/catalog/employees-data/
+    path('employees/', employee_list, name='employees'),  # http://127.0.0.1:8000/catalog/employees/
 ]
--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,34 +1,9 @@
-from django.db.models import Q
 from django.shortcuts import render
 
 from .models import Employee
 
 
-def employee_hierarchy(request):
-    employees = Employee.objects.all()
-    return render(request, 'catalog/employees.html', {'employees': employees})
-
-
 def employee_list(request):
     employees = Employee.objects.all()
-    sort_by = request.GET.get('sort_by', 'full_name')
-    employees = employees.order_by(sort_by)
-    return render(request, 'catalog/employee_list.html', {'employees': employees})
 
-
-def employee_search(request):
-    employees = Employee.objects.all()
-    search_query = request.GET.get('search_query')
-    if search_query:
-        employees = employees.filter(
-            Q(full_name__icontains=search_query) |
-            Q(hire_date__icontains=search_query) |
-            Q(salary__icontains=search_query)
-        )
-    return render(request, 'catalog/employee_list.html', {'employees': employees})
-
-
-def employee_data(request):
-    employees = Employee.objects.all()
-
-    return render(request, 'catalog/employee-data.html', {'employees': employees})
+    return render(request, 'catalog/employees.html', {'employees': employees})
